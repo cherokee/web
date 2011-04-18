653d3906ac78488c9e697320d98890988f0830d1
alobbs <alvaro@alobbs.com>
2011-04-18 18:05:37 +0000

    WIP
    git-svn-id: svn://cherokee-project.com/web/branches/beta-ctk@6536 5dc97367-97f1-0310-9951-d761b3857238

--- PageDistro.py
+++ PageDistro.py
@@ -40,6 +40,24 @@ deploying Web applications."""
 P2 = """It is specially interesting to notice that the project is
 driven by its community. Everybody is welcome to join in!"""
 
+JOIN_P1 = """Independent maintainers/packagers and ISVs are welcome to
+join this effort. If you know of any Web application you'd like to add
+to the Cherokee Distribution, please, do not hesitate to <a
+href="http://lists.octality.com/listinfo/distribution"
+target="_blank">drop us a line</a>.
+"""
+
+JOIN_P2 = """It's fairly straightforward to add a new package to the distribution:"""
+
+JOIN_L1 = {
+    'Initial Check':       """Check that the application is either <i>Orphan</i> or has not been packaged yet. You'll find a list of orphan application on this page.""",
+    'Create the package':  """We have written a few tutorials on how to create a package. Find then on the <a href="/doc">documentation</a>. In case you had any question, do not hesitate to ask on the <a href="http://lists.octality.com/listinfo/distribution" target="_blank">Distribution mailing list</a>.""",
+    'Submit your package': """Submit us the source code directory of your package. The Distribution mailing list will be again the right place."""
+}
+
+JOIN_P3 = """Once your package make it to the distribution, you will
+receive a repository account, so you can maintain it in the future."""
+
 MAINTAINERS_P1 = """A number of maintainers already work on this
 project in order to brind you these packages. We'd like to take the
 opportunity to thank them from these lines:"""
@@ -59,9 +77,20 @@ class Distribution:
         page.sidebar += CTK.RawHTML ("<h2>Apps</h2>")
         page.sidebar += Distro.Apps_List()
 
+        # Join
+        page += CTK.RawHTML ("<h2>Join the Maintainers Team!</h2>")
+        page += CTK.RawHTML ("<p>%s</p>" %(JOIN_P1))
+        page += CTK.RawHTML ("<p>%s</p>" %(JOIN_P2))
+
+        l = CTK.List()
+        page += l
+        for k in JOIN_L1:
+            l += CTK.RawHTML ("<b>%s</b>: %s"%(k, JOIN_L1[k]))
+
+        page += CTK.RawHTML ("<p>%s</p>" %(JOIN_P3))
+
         # Maintainers
         page += CTK.RawHTML ("<h2>Maintainer</h2>")
-        page += CTK.RawHTML ("<p>%s</p>" %(MAINTAINERS_P1))
         page += CTK.RawHTML ("<p>%s</p>" %(MAINTAINERS_P1))
         page += Distro.Authors_List()
 
