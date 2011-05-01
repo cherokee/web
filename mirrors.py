6931b9028c868ea0d4fdf74f00af6f4f4751f990
alobbs <alvaro@alobbs.com>
2011-05-01 15:46:31 +0000

    Adds some code for handling the list of mirror sites
    git-svn-id: svn://cherokee-project.com/web/branches/beta-ctk@6567 5dc97367-97f1-0310-9951-d761b3857238

---
+++ mirrors.py
@@ -0,0 +1,168 @@
+# -*- coding: utf-8 -*-
+
+main_site = {
+    'name': "Cherokee Project",
+    'code': 'eu',
+    'http': "http://www.cherokee-project.com/download/"
+}
+
+mirror_list = (
+    # Australia
+    {'name': "Australia's Research and Education Network",
+     'code': "au",
+     'http': "http://mirror.aarnet.edu.au/pub/cherokee/"},
+
+    # Belgium
+    {'name': "Easynet Global Services",
+     'code': "be",
+     'http': "http://ftp.easynet.be/ftp/cherokee/"},
+
+    # Canada
+    {'name': "University of Calgary",
+     'code': "ca",
+     'http': "http://mirror.cpsc.ucalgary.ca/mirror/cherokee/"},
+
+    # China
+    {'name': "PHP China",
+     'code': "cn",
+     'http': "http://cherokee.phpchina.com/"},
+    {'name': "KingIDC",
+     'code': "cn",
+     'http': "http://cherokee.kingidc.net/"},
+
+    # France
+    {'name': "Process Evolution",
+     'code': "fr",
+     'http': "http://www.cherokee.process-evolution.fr/"},
+
+    # Germany
+    {'name': "Cherokee Germany",
+     'code': "de",
+     'http': "http://www.cherokee-project.de/mirrors/cherokee/"},
+    {'name': "Friedrich-Alexander-Universität Erlangen-Nürnberg",
+     'code': "de",
+     'http': "http://ftp.uni-erlangen.de/pub/mirrors/cherokee/"},
+
+    # Greece
+    {'name': "National Technical University of Athens",
+     'code': "gr",
+     'http': "http://ftp.ntua.gr/pub/www/cherokee/"},
+    {'name': "University of Crete",
+     'code': "gr",
+     'http': "http://ftp.cc.uoc.gr/mirrors/cherokee/"},
+
+    # India
+    {'name': "Ensine Knowledge Systems",
+     'code': "in",
+     'http': "http://cherokee.technocozy.com/"},
+
+    # Indonesia
+    {'name': "Institut Teknologi Bandung",
+     'code': "id",
+     'http': "http://cherokee.labs.itb.ac.id/"},
+    {'name': "Mirrornya Apung (IIX)",
+     'code': "id",
+     'http': "http://mirror.apung.web.id/cherokee"},
+
+    # Ireland
+    {'name': "Ireland's National E&R Network",
+     'code': "ie",
+     'http': "http://ftp.heanet.ie/mirrors/cherokee/"},
+
+    # Italy
+    {'name': "The Italian Academic &amp; Research Network",
+     'code': "it",
+     'http': "http://cherokee.mirror.garr.it/mirrors/cherokee/"},
+
+    # Japan
+    {'name': "KDDI R&amp;D Labs INC.",
+     'code': "jp",
+     'http': "http://www.ftp.ne.jp/infosystems/cherokee/"},
+    {'name': "The Ring Server Project",
+     'code': "jp",
+     'http': "http://www.ring.gr.jp/archives/net/cherokee/"},
+    {'name': "Yamagata University",
+     'code': "jp",
+     'http': "http://ftp.yz.yamagata-u.ac.jp/pub/network/cherokee/"},
+
+    # Korea
+    {'name': "yongbok.net",
+     'code': "kr",
+     'http': "http://mirror.yongbok.net/cherokee/"},
+
+    # Poland
+    {'name': "Cherokee Polska",
+     'code': "pl",
+     'http': "http://cherokee-project.pl/mirror/cherokee/"},
+    {'name': "University of Warsaw",
+     'code': "pl",
+     'http': "http://ftp.icm.edu.pl/packages/cherokee/"},
+    {'name': "Warsaw Information Technology",
+     'code': "pl",
+     'http': "http://ftp.wsisiz.edu.pl/pub/Linux/cherokee/"},
+
+    # Portugal
+    {'name': "Flesk Telecom",
+     'code': "pt",
+     'http': "http://mirrors.dominios.pt/cherokee/"},
+
+    # Romania
+    {'name': "srv.ro",
+     'code': "ro",
+     'http': "http://download.srv.ro/pub/cherokee/"},
+
+    # Russia
+    {'name': "Yandex.ru",
+     'code': "ru",
+     'http': "http://mirror.yandex.ru/mirrors/cherokee-project.com/"},
+
+    # Singapore
+    {'name': "National University of Singapore",
+     'code': "sg",
+     'http': "http://mirror.nus.edu.sg/cherokee/"},
+
+    # Slovenia
+    {'name': "Bum.si",
+     'code': "si",
+     'http': "http://cherokee.bum.si/mirror"},
+
+    # South Africa
+    {'name': "Telkom SAIX",
+     'code': "za",
+     'http': "http://ftp.saix.net/Cherokee/"},
+    {'name': "Internet Solutions",
+     'code': "za",
+     'http': "http://mirror.is.co.za/mirrors/cherokee/"},
+
+    # Spain
+    {'name': "RedIris",
+     'code': "es",
+     'http': "http://ftp.rediris.es/mirror/cherokee/"},
+
+    # The Netherlands
+    {'name': "NLUUG",
+     'code': "nl",
+     'http': "http://ftp.nluug.nl/internet/cherokee/"},
+
+    # UK
+    {'name': "Hyperial.com",
+     'code': "uk",
+     'http': "http://cherokee.hyperial.com/"},
+
+    # US
+    {'name': "Pub Crawler",
+     'code': "us",
+     'http': "http://cherokee.pubcrawler.com/"},
+    {'name': "Oregon State University Open Source Lab",
+     'code': "us",
+     'http': "http://cherokee.osuosl.org/"},
+    {'name': "University of Idaho",
+     'code': "us",
+     'http': "http://mirror.its.uidaho.edu/pub/cherokee/"},
+    {'name': "Georgia Institute of Technology",
+     'code': "us",
+     'http': "http://www.gtlib.gatech.edu/pub/cherokee/"},
+    {'name': "Secution LLC",
+     'code': "us",
+     'http': "http://mirrors.secution.com/cherokee/"}
+    )
---
+++ mirrors_checker.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+
+# -*- coding: utf-8 -*-
+#
+# Cherokee-admin
+#
+# Authors:
+#      Alvaro Lopez Ortega <alvaro@alobbs.com>
+#
+# Copyright (C) 2001-2010 Alvaro Lopez Ortega
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of version 2 of the GNU General Public
+# License as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+#
+
+import re
+import sys
+import time
+#import urllib2
+import cPickle
+import pycurl
+import cStringIO
+
+from mirrors import main_site, mirror_list
+
+def check_mirror (mirror):
+    http = mirror.get('http')
+
+    mirror['status_stamp'] = time.time()
+    mirror['up_to_date']   = False
+    mirror['latest']       = None
+    mirror['error']        = None
+
+    # HTTP request
+    res = cStringIO.StringIO()
+
+    c = pycurl.Curl()
+    c.setopt(pycurl.URL, http)
+    c.setopt(pycurl.FOLLOWLOCATION, 1)
+    c.setopt(pycurl.MAXREDIRS, 5)
+    c.setopt(pycurl.CONNECTTIMEOUT, 15)
+    c.setopt(pycurl.TIMEOUT, 25)
+    c.setopt(pycurl.NOSIGNAL, 1)
+    c.setopt(pycurl.WRITEFUNCTION, res.write)
+
+    try:
+        c.perform()
+    except:
+        mirror['error'] = 'HTTP error'
+        return
+
+    # Check latest
+    try:
+        latest = re.findall (r'LATEST_is_([\d\.]+)', res.getvalue(), re.M)[0]
+    except:
+        mirror['error'] = 'No LASTEST_is link'
+        return
+
+    mirror['latest'] = latest
+
+    # Outdated
+    if main_site.get('latest') and \
+       main_site.get('latest') != latest:
+        return
+
+    # Report version
+    mirror['up_to_date'] = True
+
+def check_mirrors():
+    global main_site
+
+    # Main site
+    check_mirror (main_site)
+
+    # Check mirrors
+    results = mirror_list[:]
+    for mirror in results:
+        # Current mirror
+        print mirror['http'],
+        sys.stdout.flush()
+
+        # HTTP Request
+        response = check_mirror (mirror)
+
+        # Report
+        if mirror['error']:
+            print mirror['error']
+        elif mirror['up_to_date']:
+            print mirror['latest'], "OK"
+        else:
+            print mirror['latest'], "Outdated"
+
+    # Save
+    cPickle.dump (results, open("mirrors.pickle", 'w+'))
+    os.system ("ls -l mirrors.pickle")
+
+if __name__ == '__main__':
+    check_mirrors()
