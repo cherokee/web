fc055edc98f1cdaaac1232e36b96c04a56949c87
Alvaro Lopez Ortega <alvaro@alobbs.com>
2011-10-12 23:32:38 +0200

    Minor configuration file update

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 DOWNLOADS_LOCAL = '/var/www/www.cherokee-project.com/download'
 DOWNLOADS_WEB   = 'http://www.cherokee-project.com/download'
 
-DOC_LOCAL    = '/var/www/www.cherokee-project.com/doc'
+DOC_LOCAL    = '/var/src/cherokee/doc'
 PROUD_PICKLE = '/var/lib/ows/proud/domains.pickle'
 
 GIT_CHEROKEE_LOCAL = '/var/src/cherokee-git-www'
