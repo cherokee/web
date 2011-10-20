d7730d9863ad1ea3a0736510dae2f6c150199c05
Alvaro Lopez Ortega <alvaro@alobbs.com>
2011-10-20 11:38:15 +0200

    Changes a path of the example conf file

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # -*- Mode: python; coding: utf-8 -*-
 
-DOWNLOADS_LOCAL = '/var/www/www.cherokee-project.com/download'
+DOWNLOADS_LOCAL = '/space/www/www.cherokee-project.com/download'
 DOWNLOADS_WEB   = 'http://www.cherokee-project.com/download'
 
 DOC_LOCAL    = '/var/src/cherokee/doc'
